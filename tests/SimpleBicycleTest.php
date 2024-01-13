3940c61aac137f04b55f9ddc52299a62cbed0260
Cristian Barbu <mr.barbu@gmail.com>
2024-01-13 07:53:53 +0000

    Test file for SimpleBicycle::class

--- tests/SimpleBicycleTest.php
+++ tests/SimpleBicycleTest.php
@@ -1,18 +1,104 @@
 <?php
 
-namespace App\Tests;
+namespace Tests\App\Bicycle;
 
-use PHPUnit\Framework\TestCase;
 use App\Bicycle\SimpleBicycle;
+use PHPUnit\Framework\TestCase;
 
 class SimpleBicycleTest extends TestCase
 {
-    /**
-     * Test the getFrame method of the SimpleBicycle class.
-     */
-    public function testGetFrame()
+    public function testCreationWithDefaultValues()
     {
-        $simpleBicycle = new SimpleBicycle('carbon');
-        $this->assertSame('carbon', $simpleBicycle->getFrame());
+        $bicycle = new SimpleBicycle();
+        $this->assertSame('aluminium', $bicycle->getFrame());
+        $this->assertSame(2, $bicycle->getWheels());
+        $this->assertSame('disc', $bicycle->getBrakes());
+        $this->assertSame('standard', $bicycle->getSeat());
+        $this->assertSame('flat', $bicycle->getHandlebar());
+    }
+
+    public function testCreationWithCustomValues()
+    {
+        $bicycle = new SimpleBicycle('steel', 2, 'drum', 'comfort', 'drop');
+        $this->assertSame('steel', $bicycle->getFrame());
+        $this->assertSame(2, $bicycle->getWheels());
+        $this->assertSame('drum', $bicycle->getBrakes());
+        $this->assertSame('comfort', $bicycle->getSeat());
+        $this->assertSame('drop', $bicycle->getHandlebar());
+    }
+
+    public function testCreationFailsWithInvalidFrame()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('Invalid frame value');
+        $bicycle = new SimpleBicycle('invalidFrame');
+    }
+
+    public function testCreationFailsWithInvalidWheels()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('Invalid number of wheels');
+        $bicycle = new SimpleBicycle('steel', 0);
+    }
+
+    public function testCreationFailsWithInvalidBrakes()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('Invalid brakes value');
+        $bicycle = new SimpleBicycle('steel', 2, 'rim');
+    }
+
+    public function testCreationFailsWithEmptySeat()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('Invalid seat value');
+        $bicycle = new SimpleBicycle('steel', 2, 'disc', '');
+    }
+
+    public function testCreationFailsWithInvalidHandlebar()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('Invalid handlebar value');
+        $bicycle = new SimpleBicycle('steel', 2, 'disc', 'comfort', 'apehanger');
+    }
+
+    public function testInvalidFrameAfterConstruction()
+    {
+        $bicycle = new SimpleBicycle();
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('Invalid frame value');
+        $bicycle->setFrame('invalidFrame');
+    }
+
+    public function testInvalidWheelsAfterConstruction()
+    {
+        $bicycle = new SimpleBicycle();
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('Invalid number of wheels');
+        $bicycle->setWheels(0);
+    }
+
+    /**
+     * All the params in the constructor will throw exceptions if invalid.
+     * This test is here to get 100% coverage.
+     */
+    public function testIsFunctional()
+    {
+        $bicycle = new SimpleBicycle('steel', 2, 'disc', 'comfort', 'drop');
+        $this->assertTrue($bicycle->isFunctional());
+    }
+
+    public function testDisplayDetails()
+    {
+        $bicycle = new SimpleBicycle('steel', 3, 'drum', 'comfort', 'drop');
+
+        $expected = "Bicycle Details:\n"
+            . "Frame: steel\n"
+            . "Wheels: 3\n"
+            . "Brakes: drum\n"
+            . "Seat: comfort\n"
+            . "Handlebar: drop\n";
+
+        $this->assertSame($expected, $bicycle->displayDetails());
     }
 }
